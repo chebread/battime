85bfb7b6f7d26e73bb75170de301ed2e1ee582a4
chebread <83535893+chebread@users.noreply.github.com>
2025-07-23 22:38:38 +0900

    feat: battery charge time feat: battery information feat: flag pkg docs: README

--- README.md
+++ README.md
@@ -6,9 +6,18 @@
 - Estimated Remaining Battery Time: Get an estimate of how much usage time you have left on your current battery charge.
 - Battery Information: View detailed information about your device's battery.
 
-## Installation
-
 ## How to use
+```shell
+battime
+```
+- When charging, Battery Charge Time is displayed in red.
+- When not charging, Remaining Battery Time is displayed in cyan.
+
+```shell
+battime -i
+```
+- Your battery information (state, capacity, voltage, charge rate) is displayed.
+
 
 ## License
 MIT LICENSE &copy; 2025 Cha Haneum
--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-// (x): 사용자의 총 배터리 용량을 가져온다.
-// (x): 사용자의 지금 배터리 충전 %를 가져온다.
-// 배터리 사용 시간(시간)은 배터리 용량(mAh) / 기기 전력 소모량(mA)
-// 기기 전력 소모량은 전압(V)과 전류(A)를 곱하여 구할 수 있습니다. 공식은 P(전력, W) = E(전압, V) x I(전류, A)
-// Power: 현재 전력 소모량 (단위: W, 와트)
-// Current: 현재 전류 (단위: A, 암페어)
-// Voltage: 현재 전압 (단위: V, 볼트)
-// 3000mAh 배터리를 사용하는 기기가 500mA를 소모한다면 3000mAh / 500mA = 6시간
-// cap: 용량
-// bat: 배터리
-// Rem: remaining
-// (0): 충전중일 때는, 완충 시간 뜨게 하기
-// (0): 배터리 감소 때는 배터리 남은 시간 뜨게 하기
-
 import (
+	"flag"
 	"fmt"
 	"math"
 
@@ -29,28 +16,63 @@ func getFirstDecimalDigit(n float64) int {
 }
 
 func main() {
-	var cyan = color.New(color.FgCyan, color.Bold).PrintfFunc() // cyan colored printf
+	// colored printf
+	var boldCyan = color.New(color.FgCyan, color.Bold).PrintfFunc() // cyan colored printf
+	var boldRed = color.New(color.FgRed, color.Bold).PrintfFunc()
+	var underlineBoldWhite = color.New(color.FgWhite, color.Bold, color.Underline).PrintfFunc()
 
+	// flags
+	var iFlag = flag.Bool("i", false, "battery informations")
+	var infoFlag = flag.Bool("info", false, "battery informations")
+	flag.Parse() // flag init
+
+	// battery
 	batteries, err := battery.GetAll()
 	if err != nil {
-		fmt.Println("Could not get battery info!")
+		fmt.Println("error: Could not get battery info!")
 		return
 	}
-
-	var batteryData = batteries[0] // batteries: 구조체
-	// var batState = batteryData.State
+	var batteryData = batteries[0]                 // batteries: 구조체
+	var batState = batteryData.State               // Charging, Discharging
 	var curBatCapMWh float64 = batteryData.Current // 현재 충전된  배터리 용량 (mWh)
-	//var fullBatCapMWh = batteryData.Full           // 최대 가용 가능 배터리 용량 (mWh)
+	var fullBatCapMWh float64 = batteryData.Full   // 최대 가용 가능 배터리 용량 (mWh)
+	var designBatCapMWh float64 = batteryData.Design
 	var powerMW float64 = batteryData.ChargeRate // 현재 전력 소모량 (mW)
 	var voltageV float64 = batteryData.Voltage
-	var curBatCapMAh float64 = curBatCapMWh / voltageV // 현재 충전된  배터리 용량 (mAh)
-	//var fullBatCapMAh float64 = fullBatCapMWh / voltageV // 최대 가용 가능 배터리 용량 (mWh)
-	var powerMA float64 = powerMW / voltageV // 현재 전력 소모량 (mA)
+	var designVoltageV float64 = batteryData.DesignVoltage
 
-	timeRem := curBatCapMAh / powerMA // 추정 배터리 잔여 시간
-	hour := int(timeRem)
-	minutes := int(60 * (float64(getFirstDecimalDigit(timeRem)) / 10))
-	// 예상 남은 배터리 시간
-	fmt.Println("Remaining Battery Time")
-	cyan("%d : %d\n", hour, minutes)
+	// -i flag
+	if *iFlag || *infoFlag {
+		underlineBoldWhite("Battery Information:\n")
+		fmt.Printf("state: %s\n", batState.String())
+		fmt.Printf("current capacity: %f mWh\n", curBatCapMWh)
+		fmt.Printf("last full capacity: %f mWh\n", fullBatCapMWh)
+		fmt.Printf("design capacity: %f mWh\n", designBatCapMWh)
+		fmt.Printf("charge rate: %f mW\n", powerMW)
+		fmt.Printf("voltage: %f V\n", voltageV)
+		fmt.Printf("design voltage: %f V\n", designVoltageV)
+	}
+
+	// 인수가 없다면
+	if flag.NFlag() == 0 {
+		switch batState.String() {
+		case battery.Charging.String():
+			// 예상 남은 충전 시간
+			remainingCapMWh := fullBatCapMWh - curBatCapMWh
+			timeRem := remainingCapMWh / powerMW
+			hour := int(timeRem)
+			minutes := int(60 * (float64(getFirstDecimalDigit(timeRem)) / 10))
+
+			fmt.Println("Battery Charge Time")
+			boldRed("%d : %d\n", hour, minutes)
+		default:
+			// 예상 남은 배터리 시간
+			timeRem := curBatCapMWh / powerMW // 추정 배터리 잔여 시간
+			hour := int(timeRem)
+			minutes := int(60 * (float64(getFirstDecimalDigit(timeRem)) / 10))
+
+			fmt.Println("Remaining Battery Time")
+			boldCyan("%d : %d\n", hour, minutes)
+		}
+	}
 }
